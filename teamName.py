0e189bc2496ef7e935016278317d768a932e8d62
Young-Joo Ryu <annajjang7272@gmail.com>
2026-07-08 23:45:30 +1200

    Best score when tested for momentum and volatility

--- teamName.py
+++ teamName.py
@@ -6,18 +6,18 @@ def getMyPosition (prcSoFar):
     global currentPos
     (nins,nt) = prcSoFar.shape
 
-    if (nt < 20):
+    if (nt < 29):
         return np.zeros(nins)
 
     #todays prices are the last column of the price matrix
     today = prcSoFar[:,-1]
 
     #takes the last 21 days of prices and calculates the returns and volatility for each instrument
-    returns = np.diff(np.log(prcSoFar[:, -20:]), axis=1)
+    returns = np.diff(np.log(prcSoFar[:, -29:]), axis=1)
     volatility = np.std(returns, axis=1) + 1e-10  # add a small value to avoid division by zero
 
     
-    lastRet = np.log(today / prcSoFar[:,-10])
+    lastRet = np.log(today / prcSoFar[:,-15])  # calculate the return from 15 days ago to today
 
     lNorm = np.sqrt(lastRet.dot(lastRet))
 
