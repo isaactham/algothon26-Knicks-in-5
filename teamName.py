1431199ca89df051d765e8509987d74cf5f86cfe
Young-Joo Ryu <annajjang7272@gmail.com>
2026-07-08 23:21:48 +1200

    Estimate volatility

--- teamName.py
+++ teamName.py
@@ -5,16 +5,25 @@ currentPos = np.zeros(nInst)
 def getMyPosition (prcSoFar):
     global currentPos
     (nins,nt) = prcSoFar.shape
-    
-    if (nt < 2):
+
+    if (nt < 21):
         return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:,-1] / prcSoFar[:,-6])
+
+    #todays prices are the last column of the price matrix
+    today = prcSoFar[:,-1]
+
+    #takes the last 21 days of prices and calculates the returns and volatility for each instrument
+    returns = np.diff(np.log(prcSoFar[:, -21:]), axis=1)
+    volatility = np.std(returns, axis=1) + 1e-10  # add a small value to avoid division by zero
+
+    
+    lastRet = np.log(today / prcSoFar[:,-6])
 
     lNorm = np.sqrt(lastRet.dot(lastRet))
 
     lastRet /= lNorm
 
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:,-1]])
+    rpos = np.array([int(x) for x in 5000 * lastRet / (today * volatility)])
     currentPos = np.array([int(x) for x in currentPos+rpos])
     return currentPos
 
