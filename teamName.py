8ebeea322c68d3abb700b9d4339c27948e2a1d33
Young-Joo Ryu <annajjang7272@gmail.com>
2026-07-12 16:41:50 +1200

    volatility

--- teamName.py
+++ teamName.py
@@ -14,6 +14,10 @@ def getMyPosition (prcSoFar):
 
     lastRet = 0.5 * ret5 + 0.3 * ret10 + 0.2 * ret20
 
+    returns = np.diff(np.log(prcSoFar[:, -21:]), axis=1)
+    vol = np.std(returns, axis=1)
+    lastRet = lastRet / (vol + 1e-8)
+
     lNorm = np.sqrt(lastRet.dot(lastRet))
     lastRet /= lNorm
     rpos = np.array([int(x) for x in 4300 * lastRet / prcSoFar[:,-1]])
