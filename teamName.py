bbaf399c43c7c2fc6bef7b541ab83f4ee6578b2c
Young-Joo Ryu <annajjang7272@gmail.com>
2026-07-12 16:39:53 +1200

    multiple lookback periods

--- teamName.py
+++ teamName.py
@@ -5,12 +5,18 @@ currentPos = np.zeros(nInst)
 def getMyPosition (prcSoFar):
     global currentPos
     (nins,nt) = prcSoFar.shape
-    if (nt < 5):
+    if (nt < 21):
         return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:,-1] / prcSoFar[:,-5])
+    
+    ret5 = np.log(prcSoFar[:, -1] / prcSoFar[:, -6])
+    ret10 = np.log(prcSoFar[:, -1] / prcSoFar[:, -11])
+    ret20 = np.log(prcSoFar[:, -1] / prcSoFar[:, -21])
+
+    lastRet = 0.5 * ret5 + 0.3 * ret10 + 0.2 * ret20
+
     lNorm = np.sqrt(lastRet.dot(lastRet))
     lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:,-1]])
+    rpos = np.array([int(x) for x in 4300 * lastRet / prcSoFar[:,-1]])
     currentPos = np.array([int(x) for x in currentPos+rpos])
     return currentPos
 
