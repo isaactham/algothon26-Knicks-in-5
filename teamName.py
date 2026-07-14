7c085776238e87d9de8278b9af8b03f741430453
skim655 <skim655@aucklanduni.ac.nz>
2026-07-14 22:03:00 +1200

    testing

--- teamName.py
+++ teamName.py
@@ -13,3 +13,4 @@ def getMyPosition (prcSoFar):
     rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:,-1]])
     currentPos = np.array([int(x) for x in currentPos+rpos])
     return currentPos
+hello
